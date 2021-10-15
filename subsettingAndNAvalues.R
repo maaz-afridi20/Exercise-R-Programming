9eb4e1303ae5a6caeda45b7e9e93ce24a874309e
maaz-afridi20 <maazafridi.ma47@gmail.com>
2021-10-15 19:53:42 +0500

    What is Subsetting

---
+++ subsettingAndNAvalues.R
@@ -0,0 +1,31 @@
+    
+
+    # Subsetting :
+
+    
+    # subsetting is the way to find out the specific data from
+    # some thing like if we have a dataset of whole pakistan and
+    # and we want to work on one city then we can do subsetting
+
+    # subsetting is just Slicing in pyth
+
+    x <- c("a", "b", "c", "d")
+    print(x[1])
+    
+    x[1:3]
+    x[x>"a"] # will show all the values above then "a"
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
